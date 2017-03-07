994b99f665569e56917ac82e6d66f201e1bbd4dd
ychoy <yanyinchoy@gmail.com>
2017-03-06 23:57:47 -0800

    updated sidebar view but data doesn't show

--- frontend/app/models/listing.js
+++ frontend/app/models/listing.js
@@ -75,7 +75,7 @@ export default DS.Model.extend({
   otherAppliancesIncluded: DS.attr('string'),
   additionalPropertyOptions: DS.attr('string'),
   socialUrl: DS.attr('string'),
-  rent_amount: DS.attr('number'),
+  rentAmount: DS.attr('number'),
   latitude: DS.attr('number'),
   longitude: DS.attr('number'),
   shoppingVenues: DS.attr('string'),
