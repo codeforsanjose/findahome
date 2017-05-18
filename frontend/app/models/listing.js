6e1dea54d987f8a9100c35e8055216a6147ab507
Yan Yin Choy <yanyinchoy@gmail.com>
2017-05-17 22:54:36 -0700

    update attribute titles

--- frontend/app/models/listing.js
+++ frontend/app/models/listing.js
@@ -2,13 +2,13 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   propertyName: DS.attr('string'),
-  address: DS.attr('string'),
+  propertyAddress: DS.attr('string'),
   latitude: DS.attr('number'),
   longitude: DS.attr('number'),
   propertyManager: DS.attr('string'),
   propertyManagementPhone: DS.attr('string'),
   propertyWebsite: DS.attr('string'),
-  type: DS.attr('string'),
+  propertyType: DS.attr('string'),
   population: DS.attr('string'),
   extremelyLowIncomeUnits: DS.attr('number'),
   veryLowIncomeUnits: DS.attr('number'),
