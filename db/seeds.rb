975886811a1224efa3f2c67cbca38160be79a922
Yan Yin Choy <yanyinchoy@gmail.com>
2017-05-19 09:57:34 -0700

    fix bug in seed file - need to include listing_id

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,9 @@
-
-# BUG to fix: Database is only seeded with the first listing. 
 Listing.destroy_all
 
 
 Listing.create([
   {
+    listing_id: 1,
     property_name: "Casa Feliz Studios",
     property_address:	"525 S.9th Street, San Jose, CA 95112",
     property_website:	"http://jsco.net/property/casa-feliz-studios/",
@@ -13,6 +12,7 @@ Listing.create([
     property_type:	"Special Needs Housing"
   },
   {
+    listing_id: 2,
     property_name: 	"Creekview Inn",
     property_address:	"965 Lundy Avenue, San Jose, CA 95133",
     property_website:	"http://www.firsthousing.com/contact/rental/",
@@ -21,6 +21,7 @@ Listing.create([
     property_type:	"Special Needs Housing"
   },
   {
+    listing_id: 3,
     property_name: 	"Avenida Espana Gardens",
     property_address:	"181 Rawls Ct., San Jose, CA 95139",
     property_website:	"http://jsco.net/property/avenida-espaa-gardens/",
@@ -29,6 +30,7 @@ Listing.create([
     property_type:	"Senior Housing Units"
   },
   {
+    listing_id: 4,
     property_name: 	"Belovida Apartments",
     property_address:	"1777 Newbury Park Dr., San Jose, CA 95133",
     property_website:	"http://www.belovidanewburypark.com/",
@@ -37,6 +39,7 @@ Listing.create([
     property_type:	"Senior Housing Units"
   },
   {
+    listing_id: 5,
     property_name: 	"Miraido Village Mixed-use Project",
     property_address:	"566 N. 6th Street, San Jose, CA 95112",
     property_website:	"http://www.miraidovillageapartments.com/san-jose-ca-apartments.asp",
@@ -45,6 +48,7 @@ Listing.create([
     property_type:	"Family Housing Units"
   },
   {
+    listing_id: 6,
     property_name: 	"Monte Alban",
     property_address:	"1324 Santee Dr., San Jose, CA 95122",
     property_website: "http://jsco.net/property/monte-alban-apartments/",
