4cff0aae531f20ad541d3d0c4dc2ea1e3061307a
Yan Yin Choy <yanyinchoy@gmail.com>
2017-05-19 09:57:23 -0700

    add listing id to listing params

--- app/controllers/api/listings_controller.rb
+++ app/controllers/api/listings_controller.rb
@@ -47,8 +47,7 @@ class Api::ListingsController < ApplicationController # rubocop:disable Metrics/
 
     # Only allow a trusted parameter "white list" through.
     def listing_params
-
-      params.require(:listing).permit(:listing_id, :property_name, :property_address,
+      params.require(:listing).permit(:id, :listing_id, :property_name, :property_address,
       :latitude, :longitude, :property_website, :property_manager, :property_management_phone,
       :property_type, :population, :extremely_low_income_units, :very_low_income_units, :moderate_income_units,
       :hud_units, :total_affordable_units)
