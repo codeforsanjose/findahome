4bcbe30cf7d1069a9033b93d8395ad454a73416a
Yan Yin Choy <yanyinchoy@gmail.com>
2017-05-19 09:59:16 -0700

    update syntax

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,56 +4,56 @@ Listing.destroy_all
 Listing.create([
   {
     listing_id: 1,
-    property_name: "Casa Feliz Studios",
-    property_address:	"525 S.9th Street, San Jose, CA 95112",
-    property_website:	"http://jsco.net/property/casa-feliz-studios/",
-    property_manager:	"John Stewart Company",
-    property_management_phone: "(408) 516-4776",
-    property_type:	"Special Needs Housing"
+    property_name: 'Casa Feliz Studios',
+    property_address:	'525 S.9th Street, San Jose, CA 95112',
+    property_website:	'http://jsco.net/property/casa-feliz-studios/',
+    property_manager:	'John Stewart Company',
+    property_management_phone: '(408) 516-4776',
+    property_type:	'Special Needs Housing'
   },
   {
     listing_id: 2,
-    property_name: 	"Creekview Inn",
-    property_address:	"965 Lundy Avenue, San Jose, CA 95133",
-    property_website:	"http://www.firsthousing.com/contact/rental/",
-    property_manager:	"EAH, Inc.",
-    property_management_phone: "(408) 254-4540",
-    property_type:	"Special Needs Housing"
+    property_name: 	'Creekview Inn',
+    property_address:	'965 Lundy Avenue, San Jose, CA 95133',
+    property_website:	'http://www.firsthousing.com/contact/rental/',
+    property_manager:	'EAH, Inc.',
+    property_management_phone: '(408) 254-4540',
+    property_type:	'Special Needs Housing'
   },
   {
     listing_id: 3,
-    property_name: 	"Avenida Espana Gardens",
-    property_address:	"181 Rawls Ct., San Jose, CA 95139",
-    property_website:	"http://jsco.net/property/avenida-espaa-gardens/",
-    property_manager:	"John Stewart Company",
-    property_management_phone: "(408) 972-5529",
-    property_type:	"Senior Housing Units"
+    property_name: 	'Avenida Espana Gardens',
+    property_address:	'181 Rawls Ct., San Jose, CA 95139',
+    property_website:	'http://jsco.net/property/avenida-espaa-gardens/',
+    property_manager:	'John Stewart Company',
+    property_management_phone: '(408) 972-5529',
+    property_type:	'Senior Housing Units'
   },
   {
     listing_id: 4,
-    property_name: 	"Belovida Apartments",
-    property_address:	"1777 Newbury Park Dr., San Jose, CA 95133",
-    property_website:	"http://www.belovidanewburypark.com/",
-    property_manager:	"EAH Housing Inc.",
-    property_management_phone: "(877) 264-2669",
-    property_type:	"Senior Housing Units"
+    property_name: 	'Belovida Apartments',
+    property_address:	'1777 Newbury Park Dr., San Jose, CA 95133',
+    property_website:	'http://www.belovidanewburypark.com/',
+    property_manager:	'EAH Housing Inc.',
+    property_management_phone: '(877) 264-2669',
+    property_type:	'Senior Housing Units'
   },
   {
     listing_id: 5,
-    property_name: 	"Miraido Village Mixed-use Project",
-    property_address:	"566 N. 6th Street, San Jose, CA 95112",
-    property_website:	"http://www.miraidovillageapartments.com/san-jose-ca-apartments.asp",
-    property_manager:	"Evans Property Management, Inc.",
-    property_management_phone: "(408) 297-0990",
-    property_type:	"Family Housing Units"
+    property_name: 	'Miraido Village Mixed-use Project',
+    property_address:	'566 N. 6th Street, San Jose, CA 95112',
+    property_website:	'http://www.miraidovillageapartments.com/san-jose-ca-apartments.asp',
+    property_manager:	'Evans Property Management, Inc.',
+    property_management_phone: '(408) 297-0990',
+    property_type:	'Family Housing Units'
   },
   {
     listing_id: 6,
-    property_name: 	"Monte Alban",
-    property_address:	"1324 Santee Dr., San Jose, CA 95122",
-    property_website: "http://jsco.net/property/monte-alban-apartments/",
-    property_manager:	"John Stewart Company",
-    property_management_phone: "(408) 286-1903",
-    property_type:	"Family Housing Units"
+    property_name: 	'Monte Alban',
+    property_address:	'1324 Santee Dr., San Jose, CA 95122',
+    property_website: 'http://jsco.net/property/monte-alban-apartments/',
+    property_manager:	'John Stewart Company',
+    property_management_phone: '(408) 286-1903',
+    property_type:	'Family Housing Units'
   }
 ])
